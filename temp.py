b80e1f0e757f7ffc667d4f8c8245cb98b752eb41
moralesja <aalberto_morales@hotmail.com>
2022-05-26 07:49:49 -0500

    superupdate

--- temp.py
+++ temp.py
@@ -1,17 +1,19 @@
 # importing all required libraries
 import requests
+import os
+from dotenv import load_dotenv
+config = load_dotenv(".env")
+
+JorgeMorales = os.getenv('JorgeMorales')
+token = os.getenv('api_token')
 
 # Function to send message
-def send_message(user_id, text):
-	# This comes from BotFather
-	bf_token = ''     
-	#url = f"https://api.telegram.org/{bf_token}/sendMessage?chat_id={user_id}&text={text}"
-	url = ""
+def send_message(user_id, text,token):
+	url = f"https://api.telegram.org/{token}/sendMessage?chat_id={user_id}&text={text}"
 	resp = requests.get(url)
 	resp.raise_for_status()
    
-
-send_message('', 'testing')
+send_message(JorgeMorales, 'an error ocurred when printing',token)
 
 
 
