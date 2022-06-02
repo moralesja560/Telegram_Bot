d1be8af12b0dfed50daacf9d6c7ff9a13e22b699
moralesja560 <aalberto_morales@hotmail.com>
2022-06-02 08:54:06 -0500

    superupdate - now pastebin working

--- bot_answer.py
+++ bot_answer.py
@@ -8,12 +8,15 @@ import sys
 import csv
 import time
 from urllib.parse import quote
+import requests
 
 load_dotenv()
 JorgeMorales = os.getenv('JorgeMorales')
 Grupo_SAP = os.getenv('SAP_LT_GROUP')
 AngelI = os.getenv('AngelI')
 token = os.getenv('api_token')
+
+#---------------------------------------telegram messaging services---------------------#
 
 def send_message(user_id, text,token):
 	global json_respuesta
@@ -31,11 +34,6 @@ def send_message(user_id, text,token):
 		json_respuesta = json.loads(cuerpo_respuesta.decode("utf-8"))
 		print("mensaje enviado exitosamente")
 
-def resource_path(relative_path):
-	""" Get absolute path to resource, works for dev and for PyInstaller """
-	base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
-	return os.path.join(base_path, relative_path)
-
 def get_Telegram_updates(offset):
 	#configuramos la pagina web 
 	request = Request(f'https://api.telegram.org/{token}/getUpdates?offset={offset}')
@@ -50,27 +48,57 @@ def get_Telegram_updates(offset):
 	bot_updates = json_respuesta['result']
 	return bot_updates
 
+#---------------------------------------telegram messaging services---------------------#
+
+#####-----------------------------------Auxiliary Functions----------------------------#
+
 #load file of already processed requests
+
+def resource_path(relative_path):
+	""" Get absolute path to resource, works for dev and for PyInstaller """
+	base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+	return os.path.join(base_path, relative_path)
 
 def load_finished():
 	with open(resource_path(r'images/last.txt'), 'r') as f:
 		last_request = f.readline()
 	return last_request
-
-
+def get_pastebin():
+	url = 'https://pastebin.com/raw/Zxr2c7kb' # url of paste
+	r = requests.get(url) # response will be stored from url
+	content = r.text  # raw text from url
+	#print(content) # prints content
+	letter_list = content.split(";")
+	return letter_list
 
 def log_finished(offset):
 	#open the file and write
 	with open(resource_path(r'images/last.txt'), 'w') as f:
 		f.write(offset)
 
+#####-----------------------------------Auxiliary Functions----------------------------#
 
+#####-----------------------------------Initial Conditions----------------------------#
 offset = int(load_finished())
 print(f"-------evento inicial:{offset}")
+control_number = 1
+letter_list = get_pastebin()
+#####-----------------------------------End of initial conditions----------------------------#
+
+
+#####-----------------------------------Loop----------------------------#
 while True:
+	#control number to keep this in count
+	control_number += 1
+	print(control_number)
+	#get pastebin updates every 30 passes or 10 minutes
+	if(control_number % 30 == 0):
+		print("pastebin updated")
+		letter_list = get_pastebin()
 	#load the last offset
 	#get the updates 
 	updates = get_Telegram_updates(offset)
+	
 	if len(updates) > 0:
 		for i in range(len(updates)):
 			print(f"-------Nuevo evento:{offset}")
@@ -88,13 +116,17 @@ while True:
 				##yes it was a message
 				#check the text user sent
 				print(f"En el update {upd_id} usuario {userID} envió el texto: {TelegramCommand}")
-				if TelegramCommand == '/info':
-					info_message = f"Developer: Ing. Jorge Alberto Morales, MA \nControls Engineer | Saltillo II Plant \nMubea Coil Springs México"
+				for i in  range(len(letter_list)):
+					if TelegramCommand in letter_list[i]:
+						info_message = str(letter_list[i+1].replace("\r\n",""))
+						break
+				if len(info_message)>0:
 					send_message(userID,quote(info_message),token)
+				else:
+					send_message(userID,quote('No entendí tu mensaje: Aún no le sé bien a esto de responder a las personas.'),token)
 				print(f"-------fin de evento:{offset}")	
 				offset += 1
 		log_finished(str(offset))
 	else:
 		print('No hay nuevas actualizaciones')
 	time.sleep(20)
-
--- pastebin.py
+++ pastebin.py
@@ -2,6 +2,14 @@ import requests # requests module of python (can be installed via pip)
 url = 'https://pastebin.com/raw/Zxr2c7kb' # url of paste
 r = requests.get(url) # response will be stored from url
 content = r.text  # raw text from url
-print(content) # prints content
-letter_list = content.split(",")
-print(letter_list[1])
+#print(content) # prints content
+letter_list = content.split(";")
+
+command = 'start'
+new_letter_list = str(letter_list)
+
+for i in  range(len(letter_list)):
+	if command in letter_list[i]:
+		print (letter_list[i+1].replace("\r\n",""))
+		break
+
